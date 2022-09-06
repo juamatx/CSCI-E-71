d6fda09ee795c97c807ef5925d675c804fd27aed
juamatx <60664640+juamatx@users.noreply.github.com>
2022-09-05 20:22:38 -0400

    Update hello-world.py

--- hello-world/hello-world.py
+++ hello-world/hello-world.py
@@ -2,11 +2,11 @@ import sys
 
 
 def main():
-    if bool(sys.argv[1]):
+    if not len(sys.argv) > 1:
         name = input('Enter your name: ')
     else:
         name = sys.argv[1]
-    print("Hello,",name+'!')
+    print("Hello,", name+'!')
 
 
 if __name__ == "__main__":
